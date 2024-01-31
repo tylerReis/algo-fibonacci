3dfdbbe4bc86f82f37887ab5225349bf00faa0fa
tylerReis <tyler.reis.j@outlook.com>
2024-01-31 15:28:14 -0500

    recursive

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-  return 0;
+  if (num === 0){
+    return 0
+  }
+  else if (num === 1){
+    return 1
+  }
+  else {
+  return fibonacci(num - 1) + fibonacci(num - 2)
+  }
 }
-
 module.exports = fibonacci;
+console.log(fibonacci(7))
+
+//test are based on a zero indexed fibonacci sequence
