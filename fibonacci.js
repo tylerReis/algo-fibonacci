a02333766971fb905fc40d3ab67c963f723832fe
tylerReis <tyler.reis.j@outlook.com>
2024-01-31 15:48:58 -0500

    for loop method

--- fibonacci.js
+++ fibonacci.js
@@ -2,10 +2,10 @@ function fibonacci(num) {
   let answer = []
   for (let i = 0; i <= num; i++){
     if (i === 0){
-      answer.push(0)
+      answer.push(i)
     } 
     else if (i === 1){
-      answer.push(1)
+      answer.push(i)
     }
     else{
       answer.push(answer[i-1] + answer[i-2])
